0077ddfe6e90569646265ca1b4c561c8ce33ec67
YooDaeWon <dbeodnjs1001@naver.com>
2025-04-15 21:53:07 +0900

    del
    skill, contact, user 폴더 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,29 @@ import { PostModule } from './post/post.module';
 import { ExperienceModule } from './experience/experience.module';
 import { ContactModule } from './contact/contact.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MemoModule } from './memo/memo.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // .env 전역 사용 설정
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: config.get<boolean>('DB_SINC'), // 개발 중에만 true로, 배포 시에는 false 권장
+        charset: 'utf8mb4',
+      }),
+    }),
     UserModule,
     SkillModule,
     ProjectModule,
@@ -22,6 +40,7 @@ import { ConfigModule } from '@nestjs/config';
     ExperienceModule,
     ContactModule,
     AuthModule,
+    MemoModule,
   ],
   controllers: [AppController],
   providers: [AppService],

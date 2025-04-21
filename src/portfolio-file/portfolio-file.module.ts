c21b33bb00dac6fe6ec3b36bece0ff7985243df5
YooDaeWon <dbeodnjs1001@naver.com>
2025-04-21 13:02:15 +0900

    Update portfolio-file.module.ts
    PortfolioFile 엔티티 typeorm 등록

--- src/portfolio-file/portfolio-file.module.ts
+++ src/portfolio-file/portfolio-file.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PortfolioFileController } from './portfolio-file.controller';
 import { PortfolioFileService } from './portfolio-file.service';
+import { PortfolioFile } from './entities/portfolio-file.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PortfolioFile])],
   controllers: [PortfolioFileController],
-  providers: [PortfolioFileService]
+  providers: [PortfolioFileService],
 })
 export class PortfolioFileModule {}
